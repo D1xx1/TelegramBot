0a4e8c22ec7b83edf1d70497302aabf599a8cd2b
gorbunoV_V <admin@d1x1.ru>
2022-04-19 19:33:56 +0700

    Userdata record
    Solve a problem with a global variable and fixed a bug that occurred when using the bot at the same time.

--- rconbot.py
+++ rconbot.py
@@ -6,41 +6,37 @@ from mcrcon import MCRcon
 
 bot = telebot.TeleBot(settings.token)
 
-def record_user_data(prise, selectLevel):
-    if not os.path.isdir('userdata'):
-        os.mkdir('UserData')
-    prevDir = os.getcwd()
-    os.chdir('UserData')
-    data = open('Data.txt','w')
-    data.write(selectLevel +' '+ prise)
-    data.close()
-    if os.path.isfile(f'{nickname}.txt'):
-        os.remove(f'{nickname}.txt')
-    os.rename('Data.txt',f'{nickname}.txt')
-    os.chdir(prevDir)
-
 @bot.message_handler(content_types=['text'])
 def donate(message):
     if message.text == '/donate':
-        bot.send_message(message.from_user.id, "Введите ваш ник на сервере")
-        bot.register_next_step_handler_by_chat_id(message.chat.id, get_nickname)
+        bot.send_message(message.from_user.id, "Введите ваш ник на сервере и желаемый уровень\nНапример: Username\n1...5")
+        bot.register_next_step_handler_by_chat_id(message.chat.id, get_info)
     else:
         bot.send_message(message.from_user.id, 'Напишите /donate')
     
-def get_nickname(message):
-    global nickname
-    nickname = message.text
-    bot.send_message(message.from_user.id, f'Ваш ник {nickname}. Укажите желаемый урвоень привилегии.')
-    bot.register_next_step_handler_by_chat_id(message.chat.id, get_level)
-
-def get_level(message):
-        selectLevel = message.text
-        if selectLevel == '1' or selectLevel == '2' or selectLevel == '3' or selectLevel == '4' or selectLevel == '5':
-            prise = settings.level[int(selectLevel)]
-            record_user_data(prise, selectLevel)
+def get_info(message):          #Этот куссок кода создаёт файл с записью данных.
+        nick_level = list(message.text.split('\n'))
+        if len(nick_level) == 2:
+            bot.send_message(message.from_user.id, f'Ник: {nick_level[0]}. Желаемый уровень: {nick_level[1]}')
+            userID = message.from_user.id
+            if not os.path.isdir('UserData'):
+                os.mkdir('UserData')
+            prevDir = os.getcwd()
+            os.chdir('UserData')
+            data = open('userinfo.txt','w')
+            data.write(nick_level[1]+'\n'+nick_level[0]+'\n'+settings.level[int(nick_level[1])])
+            data.close()
+            if os.path.isfile(f'{userID}.txt'):
+                os.remove(f'{userID}.txt')
+            os.rename('userinfo.txt',f'{userID}.txt')
+            os.chdir(prevDir)
+            bot.send_message(message.from_user.id, 'Ожидание оплаты. Напишите /check для проверки.')
+            print(userID, 'Запись создана.')
         else:
-            bot.send_message(message.from_user.id, 'Введите число 1...5')
-            bot.register_next_step_handler_by_chat_id(message.chat.id, get_level)
+            bot.send_message(message.from_user.id, f'Ошибка ввода. Попробуйте снова.')
+            bot.register_next_step_handler_by_chat_id(message.chat.id, get_info)
+
+
 
 '''mc = MCRcon(settings.ip, settings.password, port=25575)
 mc.connect()
